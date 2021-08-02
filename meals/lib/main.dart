8f11d2c0ce9ed508efeb90b2680dacd22a357a5c
Baraka Mahili <georgesmahili@gmail.com>
2021-08-02 07:21:53 -0400

    Adding meals app

---
+++ meals/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mahili Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scafold(
+      body: Center(
+        child: Text('Navigation time');
+      ),
+    );
+  }
+}
