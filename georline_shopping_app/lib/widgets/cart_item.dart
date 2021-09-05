51a97a7f5515f68916dc36fafa3f70f3ff7f3598
Baraka Mahili <georgesmahili@gmail.com>
2021-09-05 06:58:15 -0400

    Add snackbar and dialog

--- georline_shopping_app/lib/widgets/cart_item.dart
+++ georline_shopping_app/lib/widgets/cart_item.dart
@@ -33,6 +33,31 @@ class CartItem extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart, // only swipe from right to left
+      confirmDismiss: (direction) {
+        // Show a dialog asking user if they want to really delete
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        );
+        // return Future.value(true);
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
--- georline_shopping_app/lib/widgets/product_item.dart
+++ georline_shopping_app/lib/widgets/product_item.dart
@@ -84,8 +84,8 @@ class ProductItem extends StatelessWidget {
               ),
               onPressed: () {
                 cart.addItem(product.id, product.title, product.price);
-                Scaffold.of(context).hideCurrentSnackBar();
-                Scaffold.of(context).showSnackBar(
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
                     content: Text('Added ${product.title} to cart!'),
                     duration: Duration(seconds: 2),
